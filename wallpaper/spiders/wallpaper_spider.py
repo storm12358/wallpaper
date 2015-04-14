564b98085840e3104ca0ee70ac611ee900561727
蔡子文 <kevincai@angejia.com>
2015-04-14 10:27:51 +0800

    add git ignore

--- wallpaper/spiders/wallpaper_spider.py
+++ wallpaper/spiders/wallpaper_spider.py
@@ -7,16 +7,22 @@ class WallPaperSpider(Spider):
     name = "wallpaper"
     allowed_domains = ["nationalgeographic.com"]
     start_urls = [
-        "http://feeds.nationalgeographic.com/ng/photography/photo-of-the-day/"
+        "http://photography.nationalgeographic.com/photography/photo-of-the-day/"
     ]
 
     def parse(self, response):
         sel = Selector(response)
-        images = sel.xpath(".//*[@id='feedContent']/xhtml:div[3]/xhtml:div[1]/xhtml:p[3]/xhtml:em").extract()
+        images = sel.xpath(".//*[@id='content_top']/div[2]/a/img").extract()
         for image in images:
             start = image.find('src="//') + 7 
             end = image.find('.jpg"') + 4
             image = 'http://' + image[start:end]
-            print(image)
-#            urllib.urlretrieve(image, "wallpaper.jpg")
+            image = image.split('/')
+            image = image[-1]
+            image = image.split('_')
+            image_name = image[0]
+            image_id = image[1]
+            url = 'http://images.nationalgeographic.com/exposure/core_media/ngphoto/image/'+image_id+'_0_1920x1200.jpg'
+            urllib.urlretrieve(url, image_name + ".jpg")
+            urllib.urlretrieve(url, "wallpaper.jpg")
         return
